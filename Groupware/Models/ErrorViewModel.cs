32547f872f6d35accb1767c7eae029a8e0dbbb94
ShuheiOhno <inu.suika8@gmail.com>
2021-11-20 23:17:17 +0900

    initial

---
+++ Groupware/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Groupware.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+        //a
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
